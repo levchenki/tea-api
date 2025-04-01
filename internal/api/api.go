6c3f93195d0d5c2e0759396d09fb55714bff30e5
levchenki <danila.levchenki@gmail.com>
2025-04-01 23:44:27 +0300

    refactoring: API versioning

---
+++ internal/api/api.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/jmoiron/sqlx"
+	v1 "github.com/levchenki/tea-api/internal/api/v1"
+	"github.com/levchenki/tea-api/internal/config"
+)
+
+func NewRouter(cfg *config.Config, db *sqlx.DB) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	v1Router := v1.NewRouter(cfg, db)
+
+	r.Route("/api", func(r chi.Router) {
+		r.Mount("/v1", v1Router)
+	})
+
+	return r
+}
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,14 +2,9 @@ package app
 
 import (
 	"fmt"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
+	"github.com/levchenki/tea-api/internal/api"
 	"github.com/levchenki/tea-api/internal/config"
-	"github.com/levchenki/tea-api/internal/controller"
 	"github.com/levchenki/tea-api/internal/migrations"
-	"github.com/levchenki/tea-api/internal/repository/postgres"
-	"github.com/levchenki/tea-api/internal/service"
 	"github.com/levchenki/tea-api/internal/storage"
 	"log"
 	"net/http"
@@ -25,86 +20,7 @@ func Run() {
 		log.Fatal(err)
 	}
 
-	teaRepository := postgres.NewTeaRepository(db)
-	tagRepository := postgres.NewTagRepository(db)
-	userRepository := postgres.NewUserRepository(db)
-	categoryRepository := postgres.NewCategoryRepository(db)
-
-	teaService := service.NewTeaService(teaRepository, tagRepository)
-	userService := service.NewUserService(userRepository)
-	categoryService := service.NewCategoryService(categoryRepository, teaRepository)
-	tagService := service.NewTagService(tagRepository)
-
-	teaController := controller.NewTeaController(teaService)
-	categoryController := controller.NewCategoryController(categoryService)
-	tagController := controller.NewTagController(tagService)
-
-	authController := controller.NewUserController(
-		cfg.JWTSecretKey,
-		cfg.BotToken,
-		userService,
-	)
-
-	r := chi.NewRouter()
-
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300,
-	}))
-
-	r.Post("/auth", authController.Auth)
-
-	r.Route("/teas", func(r chi.Router) {
-
-		r.Group(func(r chi.Router) {
-			r.Use(authController.AuthMiddleware(false))
-			r.Get("/", teaController.GetAllTeas)
-			r.Get("/{id}", teaController.GetTeaById)
-		})
-
-		r.Group(func(r chi.Router) {
-			r.Use(authController.AuthMiddleware(true))
-			r.Post("/{id}/evaluate", teaController.Evaluate)
-
-			r.Group(func(r chi.Router) {
-				r.Use(authController.AdminMiddleware)
-				r.Post("/", teaController.CreateTea)
-				r.Delete("/{id}", teaController.DeleteTea)
-				r.Put("/{id}", teaController.UpdateTea)
-			})
-		})
-	})
-
-	r.Route("/categories", func(r chi.Router) {
-		r.Get("/{id}", categoryController.GetCategoryById)
-		r.Get("/", categoryController.GetAllCategories)
-
-		r.Group(func(r chi.Router) {
-			r.Use(authController.AuthMiddleware(true))
-			r.Use(authController.AdminMiddleware)
-			r.Post("/", categoryController.CreateCategory)
-			r.Delete("/{id}", categoryController.DeleteCategory)
-			r.Put("/{id}", categoryController.UpdateCategory)
-		})
-	})
-
-	r.Route("/tags", func(r chi.Router) {
-		r.Get("/", tagController.GetAllTags)
-
-		r.Group(func(r chi.Router) {
-			r.Use(authController.AuthMiddleware(true))
-			r.Use(authController.AdminMiddleware)
-			r.Post("/", tagController.CreateTag)
-			r.Delete("/{id}", tagController.DeleteTag)
-			r.Put("/{id}", tagController.UpdateTag)
-		})
-	})
+	r := api.NewRouter(cfg, db)
 
 	http.ListenAndServe(fmt.Sprintf(":%s", cfg.Server.Port), r)
 }
